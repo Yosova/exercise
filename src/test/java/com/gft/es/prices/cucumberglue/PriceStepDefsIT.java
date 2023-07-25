e3878b97a3401f1edb2f2c4bcb7b46587f163449
Yosova <ysvaldes@gmail.com>
2023-07-25 18:51:53 +0200

    UPD | add test

--- src/test/java/com/gft/es/prices/cucumberglue/PriceStepDefsIT.java
+++ src/test/java/com/gft/es/prices/cucumberglue/PriceStepDefsIT.java
@@ -1,5 +1,6 @@
 package com.gft.es.prices.cucumberglue;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
@@ -37,4 +38,57 @@ public class PriceStepDefsIT extends BaseStepDefs {
         assertEquals( response.jsonPath().getDouble("price"), price);
         log.info("Respond a available price in PRICES");
     }
+
+
+
+   /* @When("When Request filter data {int}, {string}, {int} is null")
+    public void whenRequestFilterDataIsNull(Integer brandId, String applicationDate, Integer productId) {
+
+        Map<String, Object> request = Map.of("brandId", brandId,  "applicationDate", applicationDate);
+        log.info("Request filter data {}", request);
+        response = given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("http://localhost:" + port + "/api/prices")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    @Then("Respond a {int}")
+    public void respondA(Integer status) {
+        assertNotNull(response.getBody());
+        assertEquals( response.jsonPath().getInt("status"), status);
+        log.info("Respond a available price in PRICES");
+    }*/
+
+
+    @Given("A invalid request")
+    public void aInvalidRequest() {
+    }
+    @When("Request filter data {int}, {string}")
+    public void whenRequestFilterData(Integer brandId, String applicationDate) {
+        Map<String, Object> request = Map.of("brandId", brandId, "applicationDate", applicationDate);
+        log.info("Request filter data {}", request);
+        response = given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("http://localhost:" + port + "/api/prices")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    @Then("Respond data {int}")
+    public void respondData(Integer status) {
+        assertNotNull(response.getBody());
+        assertEquals( response.jsonPath().getInt("code"), status);
+        log.info("Respond a available price in PRICES");
+    }
+
+
 }
--- src/test/resources/features/price.feature
+++ src/test/resources/features/price.feature
@@ -5,6 +5,34 @@ Feature: Prices
 
     Then Respond a available <price> from prices
 
+    Given A invalid request
+
+    When Request filter data <brandId>, <applicationDate>
+
+    Then Respond data 400
+    Examples:
+      | brandId | applicationDate       | productId | price |
+      | 1       | "2020-06-14T10:00:00" | 35455     | 35.5  |
+      | 1       | "2020-06-14T16:00:00" | 35455     | 25.45 |
+      | 1       | "2020-06-14T21:00:00" | 35455     | 35.5  |
+      | 1       | "2020-06-15T10:00:00" | 35455     | 30.5  |
+      | 1       | "2020-06-16T21:00:00" | 35455     | 38.95 |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     Examples:
       | brandId | applicationDate         | productId | price |
       | 1       | "2020-06-14T10:00:00"   | 35455     | 35.5  |
